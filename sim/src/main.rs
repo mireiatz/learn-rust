98881e0f67acbb886ee94f5f723ebfd2933d8746
Mireia <mireia.taboada@scaffold.digital>
2024-03-17 09:45:36 +0000

    Added functionality to keeo track of actions and verbose logic, fixed parser to handle verbose, fixed output throughtout, refactored read_tracefile and parse_memory_access

--- sim/src/main.rs
+++ sim/src/main.rs
@@ -26,6 +26,7 @@ struct CacheStats {
     hits: usize,
     misses: usize,
     evictions: usize,
+    cache_actions: Vec<String>,
 }
 
 impl Cache {
@@ -53,6 +54,7 @@ impl CacheStats {
             hits: 0,
             misses: 0,
             evictions: 0,
+            cache_actions: Vec::new(),
         }
     }
 
@@ -68,7 +70,16 @@ impl CacheStats {
         self.evictions += 1;
     }
 
-    fn print(&self) {
+    fn record_cache_action(&mut self, action: &str) {
+        self.cache_actions.push(action.to_string());
+    }
+
+    fn print(&self, verbose: bool) {
+        if verbose {
+            for action in &self.cache_actions {
+                println!("{}", action);
+            }
+        } 
         println!("hits:{}, misses:{}, evictions:{}", self.hits, self.misses, self.evictions);
     }
 }
@@ -81,20 +92,22 @@ fn parse_args(args: &[String]) -> (Cache, usize, usize, usize, String, bool) {
     let mut v = false;
 
     // loop through and handle parsed options
-    let mut opts = getopt::Parser::new(args, "hv:s:E:b:t:");
+    let mut opts = getopt::Parser::new(args, "hvs:E:b:t:");
 
     while let Some(opt) = opts.next() {
+
         match opt.unwrap() {
             Opt('h', _) => {
-                println!("Usage: path_to_cache_simulator [-hv] -s <num> -E <num> -b <num> -t <file>");
-                println!("Options:");
+                println!("Usage: ");
+                println!("path_to_cache_simulator [-hv] -s <num> -E <num> -b <num> -t <file>");
+                println!("");
                 println!("  -h         Print this help message.");
                 println!("  -v         Optional verbose flag.");
                 println!("  -s <num>   Number of set index bits.");
                 println!("  -E <num>   Number of lines per set.");
                 println!("  -b <num>   Number of block offset bits.");
                 println!("  -t <file>  Trace file.");
-                println!("\nExamples:");
+                println!("Examples:");
                 println!("  linux>  ./sim-ref -s 4 -E 1 -b 4 -t traces/yi.trace");
                 println!("  linux>  ./sim-ref -v -s 8 -E 2 -b 4 -t traces/yi.trace");
                 std::process::exit(0);
@@ -109,7 +122,6 @@ fn parse_args(args: &[String]) -> (Cache, usize, usize, usize, String, bool) {
             _ => {}
         }
     }
-
     // initialize the cache
     let cache = Cache::new(s, E, b); 
     (cache, s, E, b, t, v)
@@ -128,32 +140,33 @@ fn read_tracefile(filename: &str) -> Result<Vec<String>, std::io::Error> {
     for line in reader.lines() {
         if let Ok(line) = line {
 
-            // separate parts in each line
-            let parts: Vec<&str> = line.split_whitespace().collect();
-
-            //parse parts
-            if parts.len() >= 2 {
-                // ignore instruction accesses
-                if parts[0] != "I" {
-                    let address_parts: Vec<&str> = parts[1].split(',').collect();
-                    if address_parts.len() >= 1 {
-                        memory_accesses.push(address_parts[0].to_string());
-                    }
-                }
-            }
+            memory_accesses.push(line);
         }
     }
 
     Ok(memory_accesses)
 }
 
-fn extract_address_parts(address: &str, s: usize, b: usize) -> (usize, usize, usize) {
-    let address = u64::from_str_radix(address, 16).unwrap();
-    let block_offset = (address & ((1 << b) - 1)) as usize;
-    let set_index = ((address >> b) & ((1 << s) - 1)) as usize;
-    let tag = (address >> (s + b)) as usize;
+fn parse_memory_access(memory_access: &str, s: usize, b: usize) -> Option<(char, String, usize, usize, usize, usize)> {
 
-    (tag, set_index, block_offset)
+     let memory_access_parts: Vec<&str> = memory_access.split_whitespace().collect();
+     let operation = memory_access_parts[0].chars().next().unwrap();
+
+     if memory_access_parts.len() >= 2 && memory_access_parts[0] != "I" {
+        let address_size_parts: Vec<&str> = memory_access_parts[1].split(',').collect();
+        if address_size_parts.len() >= 2 {
+
+        let size = address_size_parts[1].parse::<usize>().unwrap();
+        let address = address_size_parts[0].to_string();
+        let decimal_address = u64::from_str_radix(address_size_parts[0], 16).unwrap();
+        let block_offset = (decimal_address & ((1 << b) - 1)) as usize;
+        let set_index = ((decimal_address >> b) & ((1 << s) - 1)) as usize;
+        let tag = (decimal_address >> (s + b)) as usize;
+
+        return Some((operation, address, size, tag, set_index, block_offset))
+        }
+    } 
+        None
 }
 
 pub fn main() {
@@ -163,64 +176,58 @@ pub fn main() {
     // run parser
     let (mut cache, s, E, b, t, v) = parse_args(&args);
 
-    // print parameters
-    println!("s: {}", s);
-    println!("E: {}", E);
-    println!("b: {}", b);
-    println!("Tracefile: {}", t);
-
-    if v {
-        println!("Verbose mode enabled.");
-    }
-
     let mut cache_stats = CacheStats::new();
 
     match read_tracefile(&t) {
         Ok(memory_accesses) => {
-            println!("Memory accesses:");
-            for address in &memory_accesses {
-                let (tag, set_index, block_offset) = extract_address_parts(address, s, b);
-                println!("Tag: {}, Set Index: {}, Block Offset: {}", tag, set_index, block_offset);
+            
+            for memory_access in &memory_accesses {
 
-                let set = &mut cache.sets[set_index];
+                    if let Some((operation, address, size, tag, set_index, block_offset)) = parse_memory_access(memory_access, s, b) {
 
-                let mut found_empty_block = false;
-                let mut evicted_block_index = 0;
+                    let set = &mut cache.sets[set_index];
 
-                for (i, line) in set.lines.iter_mut().enumerate() {
-                    let block = &mut line.blocks[0];
-                    if !block.valid {
-                        // load data into empty block
-                        block.tag = tag;
-                        block.valid = true;
-                        found_empty_block = true;
-                        break;
-                    } else {
-                        // implement eviction policy 
-                        evicted_block_index = i;
+                    let mut found_empty_block = false;
+                    let mut evicted_block_index = 0;
+                    let mut action = "";
+
+                    for (i, line) in set.lines.iter_mut().enumerate() {
+                        let block = &mut line.blocks[0];
+                        if !block.valid {
+                            // load data into empty block
+                            block.tag = tag;
+                            block.valid = true;
+                            found_empty_block = true;
+                            break;
+                        } else {
+                            // implement eviction policy 
+                            evicted_block_index = i;
+                        }
                     }
-                }
-            
-                if !found_empty_block {
-                    // evict the block 
-                    let evicted_block = &mut set.lines[evicted_block_index].blocks[0];
-                    evicted_block.tag = tag;
-                    evicted_block.valid = true;
-                    cache_stats.increment_evictions();
-                }
-            
-                if found_empty_block {
-                    println!("Cache hit");
-                    cache_stats.increment_hits();
-                } else {
-                    println!("Cache miss");
-                    cache_stats.increment_misses();
+                
+                    if !found_empty_block {
+                        // evict the block 
+                        let evicted_block = &mut set.lines[evicted_block_index].blocks[0];
+                        evicted_block.tag = tag;
+                        evicted_block.valid = true;
+                        cache_stats.increment_evictions();
+                        action = "eviction";
+                    }
+                
+                    if found_empty_block {
+                        cache_stats.increment_hits();
+                        action = "hit";
+                    } else {
+                        action = "miss";
+                        cache_stats.increment_misses();
+                    }
+                    cache_stats.record_cache_action(&format!("{} {},{} {}", operation, address, size, action));               
                 }
             }
         }
         Err(err) => eprintln!("Error reading trace file: {}", err),
     }
 
-    cache_stats.print();
+    cache_stats.print(v);
 
 }
