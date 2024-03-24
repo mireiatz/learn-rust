789e1ddf69fce357f9c6820114b7028905b0a269
Mireia <mireia.taboada@scaffold.digital>
2024-03-24 13:33:49 +0000

    applied error handling throughout and tidied up comments

--- sim/src/main.rs
+++ sim/src/main.rs
@@ -3,6 +3,7 @@ use getopt::Opt;
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
 
 struct Line {
     tag: Option<usize>,
@@ -75,27 +76,60 @@ impl CacheStats {
     }
 }
 
-// Parse command line arguments and return parameters
-fn parse_args(args: &[String]) -> (usize, usize, usize, String) {
+// Parse command-line arguments and return parameters
+fn parse_args(args: &[String]) -> Result<(usize, usize, usize, String), String> {
     let mut s = 0;
     let mut e = 0;
     let mut b = 0;
     let mut t = String::new();
 
-    // Use getopt crate
-    let mut opts = getopt::Parser::new(args, "s:E:b:t:");
+    let mut counts = HashMap::new();
+    counts.insert('s', 0);
+    counts.insert('E', 0);
+    counts.insert('b', 0);
+    counts.insert('t', 0);
+
+    let mut opts = getopt::Parser::new(args, "s:E:b:t:"); // Use getopt crate
 
     while let Some(opt) = opts.next() {
-        match opt.unwrap() {
-            Opt('s', Some(val)) => s = val.parse().unwrap(),
-            Opt('E', Some(val)) => e = val.parse().unwrap(),
-            Opt('b', Some(val)) => b = val.parse().unwrap(),
-            Opt('t', Some(val)) => t = val,
-            _ => {}
+        match opt {
+            Ok(Opt(flag, Some(val))) => {
+                let count = counts.entry(flag).or_insert(0usize);
+                *count += 1;
+                if *count > 1 {
+                    return Err(format!("duplicate flag -{}", flag));
+                }
+                match flag {
+                    't' => {
+                        t = val;
+                    }
+                    's' | 'E' | 'b' => {
+                        let param = val.parse().map_err(|e| format!("invalid value for -{} flag ({})", flag, e))?;
+                        if flag == 's' {
+                            s = param;
+                        } else if flag == 'E' {
+                            e = param;
+                        } else {
+                            b = param;
+                        }
+                    }
+                    _ => return Err(format!("unknown flag: -{}", flag)),
+                }
+            }
+            Ok(Opt(_, None)) => {
+                return Err("unexpected option".to_string());
+            }
+            Err(err) => {
+                return Err(format!("{}", err));
+            }
         }
     }
 
-    (s, e, b, t)
+    if s == 0 || e == 0 || b == 0 || t.is_empty() {
+        return Err("missing required arguments, incorrect command-line format".to_string());
+    }
+
+    Ok((s, e, b, t))
 }
 
 // Read memory access trace file and return vector of memory accesses
@@ -107,27 +141,38 @@ fn read_tracefile(filename: &str) -> Result<Vec<String>, std::io::Error> {
 }
 
 // Parse memory access string and return set index, tag, and operation
-fn parse_memory_access(memory_access: &str, s: usize, b: usize) -> Option<(usize, usize, char)> {
+fn parse_memory_access(memory_access: &str, s: usize, b: usize) -> Result<Option<(usize, usize, char)>, String> {
+    if memory_access.is_empty() { 
+        return Ok(None); 
+    }
     let memory_access_parts: Vec<&str> = memory_access.split_whitespace().collect();
 
-    if memory_access_parts.len() >= 2 && memory_access_parts[0] != "I" {
-        let operation = memory_access_parts[0].chars().next().unwrap();
+    if memory_access_parts.len() >= 2 { 
+        if memory_access_parts[0] == "I" { // Skip instruction cache accesses
+            return Ok(None);
+        }
+        let operation = match memory_access_parts[0] {
+            "S" | "M" | "L" => memory_access_parts[0].chars().next().unwrap(),
+            _ => return Err("invalid operation encountered".to_string())
+        };        
         let address_size_parts: Vec<&str> = memory_access_parts[1].split(',').collect();
         if address_size_parts.len() >= 2 {
-            let address = u64::from_str_radix(address_size_parts[0], 16).unwrap(); // Parse hexadecimal address
-            let binary_address = format!("{:0>64b}", address);
+            let hexadecimal_address = u64::from_str_radix(address_size_parts[0], 16)
+                .map_err(|e| format!("failed to parse address ({})", e))?;
+            let binary_address = format!("{:0>64b}", hexadecimal_address);
             let set_index_start = 64 - b;
-            let tag_index_start = set_index_start - s;
-            let tag = usize::from_str_radix(&binary_address[..tag_index_start], 2).unwrap();
-            let set_index =
-                usize::from_str_radix(&binary_address[tag_index_start..set_index_start], 2)
-                    .unwrap();
-            return Some((set_index, tag, operation));
+            let tag_start = set_index_start - s;
+            let tag = usize::from_str_radix(&binary_address[..tag_start], 2)
+                .map_err(|e| format!("failed to parse tag ({})", e))?;
+            let set_index = usize::from_str_radix(&binary_address[tag_start..set_index_start], 2)
+                .map_err(|e| format!("failed to parse set index ({})", e))?;
+            return Ok(Some((set_index, tag, operation)));
         }
     }
-    None
+    Err("invalid memory access format".to_string())
 }
-// Update the LRU list based on the accessed index
+
+// Update the LRU list based on the accessed line
 fn update_lru_list(cache: &mut Cache, set_index: usize, accessed_index: usize) {
     if let Some(position) = cache.sets[set_index]
         .lru_list
@@ -146,22 +191,30 @@ fn simulate_cache_access(
     operation: char,
     set_index: usize,
     tag: usize,
-) {
+) -> Result<(), String> {
     match operation {
         'L' | 'S' => {
             let mut found_empty_line = false;
 
+            if set_index >= cache.sets.len() {
+                return Err("failed to access cache set".to_string());
+            }
+
             for index in 0..cache.sets[set_index].lines.len() {
+                if index >= cache.sets[set_index].lines.len() {
+                    return Err("failed to access cache line".to_string());
+                }
+
                 if let Some(line_tag) = cache.sets[set_index].lines[index].tag {
                     // If the line is not empty, compare the tags - if they match, it's a hit
                     if line_tag == tag {
                         stats.record_hit();
                         update_lru_list(cache, set_index, index);
-                        return;
+                        return Ok(());
                     }
                 } else {
-                    // If the line is empty, update its values
-                    cache.sets[set_index].lines[index].tag = Some(tag); // Set the tag
+                    // If the line is empty, it's a miss and update tag
+                    cache.sets[set_index].lines[index].tag = Some(tag); 
                     found_empty_line = true;
                     stats.record_miss();
                     update_lru_list(cache, set_index, index);
@@ -169,24 +222,27 @@ fn simulate_cache_access(
                 }
             }
 
-            // If no hit happened and no empty line was found, evict the LRU line
+            // If no hit happened and no empty line was found, evict the LRU line - it's an eviction and update the tag
             if !found_empty_line {
                 if let Some(evict_index) = cache.sets[set_index].lru_list.first().cloned() {
-                    cache.sets[set_index].lines[evict_index].tag = Some(tag); // Set the tag
+                    cache.sets[set_index].lines[evict_index].tag = Some(tag); 
                     stats.record_miss();
                     stats.record_eviction();
                     update_lru_list(cache, set_index, evict_index);
+                    return Ok(());
                 }
-                return;
+                return Err("eviction failed".to_string());
             }
+            return Ok(());
         }
         'M' => {
             // Simulate L operation followed by S operation
-            simulate_cache_access(cache, stats, 'L', set_index, tag);
-            simulate_cache_access(cache, stats, 'S', set_index, tag);
+            simulate_cache_access(cache, stats, 'L', set_index, tag)?;
+            simulate_cache_access(cache, stats, 'S', set_index, tag)?;
+            return Ok(());
         }
         _ => {
-            eprintln!("Unknown operation: {}", operation);
+            return Err(format!("unknown operation: {}", operation));
         }
     }
 }
@@ -194,25 +250,51 @@ fn simulate_cache_access(
 pub fn main() {
     // Collect command line arguments and parse them
     let args: Vec<String> = env::args().collect();
-    let (s, e, b, t) = parse_args(&args);
+    let (s, e, b, t) = match parse_args(&args) {
+        Ok(params) => params,
+        Err(err) => {
+            eprintln!("Error parsing command-line arguments: {}", err);
+            eprintln!("Usage: -- -s <set index bits> -E <lines in set> -b <block bits> -t <tracefile>");
+            return;
+        }
+    };
 
-    // Initialize the cache, stats and counter
+    // Initialize the cache and stats
     let mut cache = Cache::new(s, e);
     let mut stats = CacheStats::new();
 
     // Read tracefile and loop through memory accesses
     match read_tracefile(&t) {
         Ok(memory_accesses) => {
-            for address in &memory_accesses {
+            for memory_access in &memory_accesses {
+
                 // Simulate cache behaviour for each memory access
-                if let Some((set_index, tag, operation)) = parse_memory_access(address, s, b) {
-                    simulate_cache_access(&mut cache, &mut stats, operation, set_index, tag);
+                match parse_memory_access(memory_access, s, b) {
+                    Ok(Some((set_index, tag, operation))) => {
+                        match simulate_cache_access(&mut cache, &mut stats, operation, set_index, tag) {
+                            Ok(_) => {}
+                            Err(err) => {
+                                eprintln!("Error simulating cache access: {}", err);
+                                return;
+                            }
+                        }
+                    }
+                    Ok(None) => {
+                        continue
+                    }
+                    Err(err) => {
+                        eprintln!("Error parsing memory access: {}", err);
+                        return; 
+                    }
                 }
             }
-
-            // Print results
-            stats.print_stats();
         }
-        Err(err) => eprintln!("Error reading trace file: {}", err),
+        Err(err) => {
+            eprintln!("Error reading tracefile {}: {}", t, err);
+            return;
+        }
     }
+
+    // Print results
+    stats.print_stats();
 }
